e5199165083f0688dfe183b74556f7422a5f7131
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2022-02-17 08:21:19 +0100

    create test for blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        """use model specifications to setup database"""
+        self.blog = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        """Test creating a new post model"""
+        d = self.blog
+
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
